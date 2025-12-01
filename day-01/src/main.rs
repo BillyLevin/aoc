7db0127d504442ca368b375d7897a7cec94e5ee2
Billy Levin <billy.levin97@gmail.com>
2025-12-01 07:25:25 +0000

    init with day 1

---
+++ day-01/src/main.rs
@@ -0,0 +1,102 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    println!("Part one: {}", part_one(input));
+
+    let input = include_str!("../input.txt");
+    println!("Part two: {}", part_two(input));
+}
+
+fn part_one(input: &str) -> u64 {
+    let mut result = 0;
+    let mut dial: i32 = 50;
+
+    for line in input.lines() {
+        let (direction, count) = line.split_at(1);
+        let count: i32 = count.parse().expect("count should be a number");
+
+        match direction {
+            "R" => dial += count,
+            "L" => dial -= count,
+            _ => unreachable!("direction should be `R` or `L`"),
+        }
+
+        dial %= 100;
+
+        if dial == 0 {
+            result += 1;
+        }
+    }
+
+    result
+}
+
+fn part_two(input: &str) -> u64 {
+    let mut result = 0;
+    let mut dial: i32 = 50;
+
+    for line in input.lines() {
+        let (direction, count) = line.split_at(1);
+        let count: i32 = count.parse().expect("count should be a number");
+
+        match direction {
+            "R" => {
+                for _ in 0..count {
+                    dial += 1;
+                    dial %= 100;
+                    if dial == 0 {
+                        result += 1;
+                    }
+                }
+            }
+            "L" => {
+                for _ in 0..count {
+                    dial -= 1;
+                    dial %= 100;
+                    if dial == 0 {
+                        result += 1;
+                    }
+                }
+            }
+            _ => unreachable!("direction should be `R` or `L`"),
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_example() {
+        let input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+
+        assert_eq!(part_one(input), 3);
+    }
+
+    #[test]
+    fn part_two_example() {
+        let input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+
+        assert_eq!(part_two(input), 6);
+    }
+}
